e194885bd72392169b83ed3d21a3c2042c2b7aed
Bruno Mota <brunomottta@terra.com.br>
2020-07-25 22:47:54 -0300

    Java courses

--- exercicio_array/src/entities/Product.java
+++ exercicio_array/src/entities/Product.java
@@ -10,16 +10,13 @@ public class Product {
 		this.price = price;
 	}
 
-
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 
 	public double getPrice() {
 		return price;
